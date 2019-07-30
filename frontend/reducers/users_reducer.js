420a18c8550bab263cad98c643b5c205429935b7
Chung Chan <Chungyyy@hotmail.com>
2019-07-30 18:05:00 -0400

    add user reducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,19 @@
+import {
+  RECEIVE_CURRENT_USER,
+} from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const usersReducer = (state, action) => {
+  Object.freeze(state);
+  let newState;
+
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      newState = merge({}, state, { [action.currentUser.id]: action.currentUser });
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
