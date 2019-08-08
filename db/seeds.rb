6c3368a3bb7bbc251f0401f830cbfd9acd080283
Chung Chan <Chungyyy@hotmail.com>
2019-08-08 15:33:53 -0400

    bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ require 'open-uri'
 # matrix = Video.new(title: "", description: "", maturity_rating: "", year: 1999)
 # captain_marvel.movie.attach(io: open('https://s3.amazonaws.com/nextup-seed/Marvel+Studios+Captain+Marvel+-+Official+Trailer.mp4'), filename: 'Captain Marveltrailer.mp4')
 # watchmen2 = Video.create(title: "watchmen_sample", description: "sample", maturity_rating: "R", year: 2009)
-# watchmen = Video.find(3)
-# watchmen.video.attach(io: open('s3://netflicks-seed/watchmen_trailer.mp4'), filename: 'watchmen_trailer.mp4')
+watchmen = Video.find(3)
+watchmen.video.attach(io: open('s3://netflicks-seed/watchmen_trailer.mp4'), filename: 'watchmen_trailer.mp4')
 
 # bunnyMovie = Video.create!(title: "Bunny Movie", description: "Bunny Movie", maturity_rating: "G", year: 2222)
 # bunnyMovie.video.attach(io: open('http://media.w3.org/2010/05/bunny/movie.mp4'), filename: 'movie.mp4')
