c923dfc8db93ce374610f954ab2521e311d0f1bc
Chung Chan <Chungyyy@hotmail.com>
2019-12-13 14:41:00 -0500

    implement actions and thunk action creators

--- frontend/actions/video_actions.js
+++ frontend/actions/video_actions.js
@@ -5,6 +5,14 @@ export const RECEIVE_VIDEO = 'RECEIVE_VIDEO';
 export const RECEIVE_SEARCHED_VIDEOS = 'RECEIVE_SEARCHED_VIDEOS';
 export const CLEAR_SEARCHED_VIDEOS = 'CLEAR_SEARCHED_VIDEOS';
 
+export const RECEIVE_WATCH_LIST_VIDEOS = 'RECEIVE_WATCH_LIST_VIDEOS';
+export const ADD_WATCH_LIST_VIDEO = 'ADD_WATCH_LIST_VIDEO';
+export const DELETE_WATCH_LIST_VIDEO = 'DELETE_WATCH_LIST_VIDEO';
+
+// VIDEOS
+// VIDEOS
+// VIDEOS
+
 export const fetchAllVideos = () => dispatch => {
   return video_util.fetchVideos().
     then(videos => dispatch(receiveVideos(videos)));
@@ -20,21 +28,21 @@ export const fetchSearchedVideos = (query) => dispatch => {
     then(videos => dispatch(receiveSearchedVideos(videos)));
 }
 
-const receiveVideos = (videos) => {
+export const receiveVideos = (videos) => {
   return {
     type: RECEIVE_ALL_VIDEOS,
     videos,
   };
 };
 
-const receiveVideo = (video) => {
+export const receiveVideo = (video) => {
   return {
     type: RECEIVE_VIDEO,
     video,
   };
 };
 
-const receiveSearchedVideos = (videos) => {
+export const receiveSearchedVideos = (videos) => {
   return {
     type: RECEIVE_SEARCHED_VIDEOS,
     videos,
@@ -46,3 +54,43 @@ export const clearSearchedVideos = () => {
     type: CLEAR_SEARCHED_VIDEOS,
   };
 };
+
+// WATCHLIST VIDEOS
+// WATCHLIST VIDEOS
+// WATCHLIST VIDEOS
+
+export const fetchWatchListVideos = () => dispatch => {
+  return video_util.fetchWatchListVideos().
+    then(videos => dispatch(receiveWatchListVideos(videos)));
+}
+
+export const addWatchListVideo = (id) => dispatch => {
+  return video_util.addWatchListVideo(id).
+    then(videos => dispatch(addToList(videos)));
+}
+
+export const deleteWatchListVideo = (id) => dispatch => {
+  return video_util.deleteWatchListVideo(id).
+    then(videos => dispatch(deleteFromList(videos)));
+}
+
+export const receiveWatchListVideos = (videos) => {
+  return {
+    type: RECEIVE_WATCH_LIST_VIDEOS,
+    videos,
+  };
+}
+
+export const addToList = (videos) => {
+  return {
+    type: ADD_WATCH_LIST_VIDEO,
+    videos,
+  };
+}
+
+export const deleteFromList = (videos) => {
+  return {
+    type: DELETE_WATCH_LIST_VIDEO,
+    videos,
+  };
+}
