587756a9a4e4b74845c905ce270ff6bd22b0ac62
Chung Chan <Chungyyy@hotmail.com>
2020-01-20 14:34:28 -0500

    audio button added to main video

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import VideoRow from './video_row';
-import Header from '../header/header_container';
-import { Link, withRouter } from 'react-router-dom';
-import Search from '../search/search_container';
+import React from "react";
+import VideoRow from "./video_row";
+import Header from "../header/header_container";
+import { Link, withRouter } from "react-router-dom";
+import Search from "../search/search_container";
 
 class Main extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       initialScrollPos: 0,
       headerPinned: false,
       searching: false,
       viewingWatchlist: false,
+      muted: false,
+      prevVolume: 0.5,
+      volume: 0.5
     };
 
     this.handleScroll = this.handleScroll.bind(this);
     this.notSearching = this.notSearching.bind(this);
     this.isSearching = this.isSearching.bind(this);
     this.audioOn = this.audioOn.bind(this);
-
+    this.handleMute = this.handleMute.bind(this);
   }
 
   componentDidMount() {
@@ -27,24 +30,40 @@ class Main extends React.Component {
     console.log("component mounting");
     this.props.fetchWatchListVideos();
     document.addEventListener("scroll", this.handleScroll);
-  };
+  }
 
   componentWillUnmount() {
     document.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleMute() {
+    if (this.state.muted) {
+      this.setState({
+        muted: false,
+        volume: this.state.prevVolume
+      });
+      this.refs.vidRef.volume = this.state.prevVolume;
+    } else {
+      this.setState({
+        muted: true,
+        volume: 0
+      });
+      this.refs.vidRef.volume = 0;
+    }
   }
 
   handleScroll() {
     const scrollPos = window.pageYOffset;
     const headerPinned = scrollPos > this.state.initialScrollPos;
     this.setState({
-      headerPinned,
+      headerPinned
     });
-  };
+  }
 
   isSearching(query) {
     this.setState({ searching: query });
   }
-  
+
   notSearching() {
     this.setState({ searching: false });
   }
@@ -60,7 +79,9 @@ class Main extends React.Component {
 
     const logoutButton = (
       <div>
-        <span onClick={this.props.logout} className="logout-text">Sign out of Netflicks</span>
+        <span onClick={this.props.logout} className="logout-text">
+          Sign out of Netflicks
+        </span>
         <span className="logout-text">More Options Soon &trade;</span>
       </div>
     );
@@ -75,15 +96,28 @@ class Main extends React.Component {
         key={this.props.watchmenVideoClip}
         poster="https://www.syfy.com/sites/syfy/files/styles/1200x680/public/watchmen_2_0.jpg"
         src={this.props.watchmenVideoClip}
-      >
-      </video>
-    )
+      ></video>
+    );
+
+    const audioButton = this.state.muted ? (
+      <i
+        id="main-page-volume"
+        className="fas fa-volume-mute"
+        onClick={this.handleMute}
+      ></i>
+    ) : (
+      <i
+        id="main-page-volume"
+        className="fas fa-volume-up"
+        onClick={this.handleMute}
+      ></i>
+    );
 
     const main = this.state.searching ? (
       <div className="search-container">
         <Search />
       </div>
-      ) : (
+    ) : (
       <div className="index-ctn">
         <div className="main-view">
           <div className="main-video-wrapper">
@@ -98,8 +132,8 @@ class Main extends React.Component {
                     <span id="play-button">Play</span>
                   </button>
                 </Link>
+                {audioButton}
               </div>
-
             </div>
             {/* <img className="static-image" src="https://www.syfy.com/sites/syfy/files/styles/1200x680/public/watchmen_2_0.jpg" /> */}
             {mainVideo}
@@ -112,19 +146,36 @@ class Main extends React.Component {
           <VideoRow videos={this.props.videos} />
         </div>
         <p className="genre-text">Action</p>
-        <VideoRow videos={this.props.videos.filter(video => video.genres.includes("Action"))} />
+        <VideoRow
+          videos={this.props.videos.filter(video =>
+            video.genres.includes("Action")
+          )}
+        />
         <p className="genre-text">Drama</p>
-        <VideoRow videos={this.props.videos.filter(video => video.genres.includes("Drama"))} />
+        <VideoRow
+          videos={this.props.videos.filter(video =>
+            video.genres.includes("Drama")
+          )}
+        />
         <p className="genre-text">Musicals</p>
-        <VideoRow videos={this.props.videos.filter(video => video.genres.includes("Musicals"))} />
+        <VideoRow
+          videos={this.props.videos.filter(video =>
+            video.genres.includes("Musicals")
+          )}
+        />
         <p className="genre-text">Thriller</p>
-        <VideoRow videos={this.props.videos.filter(video => video.genres.includes("Thriller"))} />
+        <VideoRow
+          videos={this.props.videos.filter(video =>
+            video.genres.includes("Thriller")
+          )}
+        />
         <p className="genre-text">Adventure</p>
-        <VideoRow videos={this.props.videos.filter(video => video.genres.includes("Adventure"))} />
-        <div className="index-footer">
-          {/* TODO */}
-        </div>
-
+        <VideoRow
+          videos={this.props.videos.filter(video =>
+            video.genres.includes("Adventure")
+          )}
+        />
+        <div className="index-footer">{/* TODO */}</div>
       </div>
     );
 
@@ -138,8 +189,8 @@ class Main extends React.Component {
         />
         {main}
       </div>
-    )
+    );
   }
-};
+}
 
 export default withRouter(Main);
